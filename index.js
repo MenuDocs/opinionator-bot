3bdd33193f829b4bc55143d0e011c9cb6fe5f198
Connor Strand <42255832+Strandxo@users.noreply.github.com>
2019-12-31 00:22:05 +1030

    test

--- index.js
+++ index.js
@@ -10,44 +10,46 @@ client.on("ready", () => {
 
 client.on("message", async ({ author, guild, content, channel }) => {
     if (author.bot || !content.startsWith(prefix) || !guild) return;
-    
+
     const args = content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
 
-    switch(cmd) {
+    switch (cmd) {
         case "ops":
             const category = guild.channels.find(category => category.type === "category" && category.name === "ops");
             if (!category) return channel.send("Please create an `ops` category.");
-            
+
             const botCom = guild.channels.get("660858981396381708");
             if (channel !== botCom) return channel.send("You can only create ops in <#660858981396381708>");
-            
+
             const regex = new RegExp("^(http|https)://", "i");
             if (!args[0] || !regex.test(args[0])) return channel.send("Please provide a valid url to a project you wish to obtain opinions on.");
-            
-            const opChan = guild.channels.find(channel => channel.name === `ops-${author.username}`);
-            if (opChan) return opChan.send(`[ everyone ] Please share your opinions on this: ${args[0]}`);
-            
-            const schannel = await guild.createChannel(`ops-${author.username}`, { type: "textchannel" }).catch(console.error);
-            await schannel.setParent(category.id).catch(console.error);
 
-            schannel.send(`[ everyone ] Please share your opinions on this: ${args[0]}`);
-        break;
+            const opChan = guild.channels.find(channel => channel.topic === author.id);
+            if (opChan) return opChan.send(`[ everyone ] Please share your opinions on this: ${args[0]}`);
+
+            const sChannel = await guild.createChannel(`ops-${author.username}`, { 
+                type: "textchannel", 
+                parent: category.id, 
+                topic: author.id 
+            }).catch(console.error);
+
+            sChannel.send(`[ everyone ] Please share your opinions on this: ${args[0]}`);
+            break;
 
         case "opsclose":
             if (!["ops"].some(category => channel.name.includes(category))) return channel.send("This isnt an ops channel.");
             channel.delete();
-        break;
+            break;
 
         case "help":
             channel.send(stripIndents`
             Commands:
             **[ $ops ]** - Creates channel for opinions and pings the developer role.
             **[ $opsclose ]** - Deletes channel the command is invoked in.
-
             ( Developed by Connor#4767 )
             `);
-        break;
+            break;
     }
 
 });
