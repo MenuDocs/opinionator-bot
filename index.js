5444725d4e17551ef1d07ba2b0238db5c6d83318
Connor Strand <42255832+Strandxo@users.noreply.github.com>
2019-12-31 00:03:12 +1030

    bug fix

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ client.on("message", async ({ author, guild, content, channel }) => {
     
     const args = content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
-    
-    const botCom = guild.channels.get("660858981396381708");
-    if(channel !== botCom) return channel.send("You can only invoke commands in <#660858981396381708>");
 
     switch(cmd) {
         case "ops":
             const category = guild.channels.find(category => category.type === "category" && category.name === "ops");
             if (!category) return channel.send("Please create an `ops` category.");
+            
+            const botCom = guild.channels.get("660858981396381708");
+            if(channel !== botCom) return channel.send("You can only create ops in <#660858981396381708>");
             
             const regex = new RegExp("^(http|https)://", "i");
             if (!args[0] || !regex.test(args[0])) return channel.send("Please provide a valid url to a project you wish to obtain opinions on.");
